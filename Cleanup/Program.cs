00b83a45a6f9dd0155d775f10c9b4b24c5715b38
Leonid Voitko <zanleo.z@gmail.com>
2024-10-07 22:25:45 +0200

    Refactoring and small optimization

--- Cleanup/Program.cs
+++ Cleanup/Program.cs
@@ -1,11 +1,12 @@
-﻿namespace Cleanup
+﻿using System;
+
+namespace Cleanup
 {
     internal class Program
     {
         private const double TargetChangeTime = 1;
 
         private double _previousTargetSetTime;
-        private bool _isTargetSet;
         private dynamic _lockedCandidateTarget;
         private dynamic _lockedTarget;
         private dynamic _target;
@@ -13,78 +14,96 @@
         private dynamic _activeTarget;
         private dynamic _targetInRangeContainer;
 
-        public void CleanupTest(Frame frame)
+        private dynamic Target
         {
-            try
+            set
             {
-                if (_lockedCandidateTarget && !_lockedCandidateTarget.CanBeTarget)
+                if (_previousTarget == _target)
                 {
-                    _lockedCandidateTarget = null;
+                    return;
                 }
-
-                if (_lockedTarget && !_lockedTarget.CanBeTarget)
-                {
-                    _lockedTarget = null;
-                }
-
-                _isTargetSet = false;
-				// Sets _activeTarget field
-                TrySetActiveTargetFromQuantum(frame);
-
-                // If target exists and can be targeted, it should stay within Target Change Time since last target change
-                if (_target && _target.CanBeTarget && Time.time - _previousTargetSetTime < TargetChangeTime)
-                {
-                    _isTargetSet = true;
-                }
+                
                 _previousTarget = _target;
-
-                if (!_isTargetSet)
+                _target = value;
+                if (IsValidTarget(_target))
                 {
-                    if (_lockedTarget && _lockedTarget.CanBeTarget)
-                    {
-                        _target = _lockedTarget;
-                        _isTargetSet = true;
-                        return;
-                    }
-                }
-
-
-                if (!_isTargetSet)
-                {
-                    if (_activeTarget && _activeTarget.CanBeTarget)
-                    {
-                        _target = _activeTarget;
-                        _isTargetSet = true;
-                        return;
-                    }
-                }
-
-                if (!_isTargetSet)
-                {
-                    _target = _targetInRangeContainer.GetTarget();
-                    if (_target)
-                    {
-                        _isTargetSet = true;
-                    }
-                }
-            }
-            finally
-            {
-                if (_isTargetSet)
-                {
-                    if (_previousTarget != _target)
-                    {
-                        _previousTargetSetTime = Time.time;
-                    }
-                }
-                else
-                {
-                    _target = null;
+                    _previousTargetSetTime = Time.time;
                 }
                 TargetableEntity.Selected = _target;
             }
         }
 
+        public void CleanupTest(Frame frame)
+        {
+            TryResetTarget(ref _lockedCandidateTarget, CanNotBeTarget);
+            TryResetTarget(ref _lockedTarget, CanNotBeTarget);
+
+			// Sets _activeTarget field
+            // Сделал допущение!!! что тут не может быть Exception, тогда нам не нужен блок try/finally
+            TrySetActiveTargetFromQuantum(frame);
+
+            UpdateTarget();
+        }
+
+        private void UpdateTarget()
+        {
+            // If target exists and can be targeted, it should stay within Target Change Time since last target change
+            if (CanBeTarget(_target) && IsPrevieousTargetSetTimeValid())
+            {
+                return;
+            }
+            
+            // Если бы не dynamic - я бы использовал метод с такой сигнатурой , чтобы сделать и проверку и установку цель
+            // private bool TrySetTarget(dynamic target, Func<dynamic, bool> checkForTarget = null)
+            // Выглядело бы так
+            // if (TrySetTarget(_lockedTarget, CanBeTarget)) return;
+            if (CanBeTarget(_lockedTarget))
+            {
+                Target = _lockedTarget;
+                return;
+            }
+            
+            if (CanBeTarget(_activeTarget))
+            {
+                Target = _activeTarget;
+                return;
+            }
+
+            // допускаю, что GetTarget() возвращает тут null если нету цели
+            Target = _targetInRangeContainer.GetTarget();
+        }
+
         // MORE CLASS CODE
+
+        private bool IsPrevieousTargetSetTimeValid()
+        {
+            return Time.time - _previousTargetSetTime < TargetChangeTime;
+        }
+
+        private bool IsValidTarget(dynamic target)
+        {
+            return target;
+        }
+        
+        private bool CanBeTarget(dynamic target)
+        {
+            return IsValidTarget(target) && target.CanBeTarget;
+        }
+        
+        private bool CanNotBeTarget(dynamic target)
+        {
+            return IsValidTarget(target) && !target.CanBeTarget;
+        }
+        
+        private bool TryResetTarget(ref dynamic lockedTarget, Func<dynamic, bool> checkForReset)
+        {
+            if (!checkForReset(lockedTarget))
+            {
+                return false;
+            }
+            
+            lockedTarget = null;
+            return true;
+        }
     }
 }
